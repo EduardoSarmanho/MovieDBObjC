0c979067b042819857b2c2249d9d6f0df6fe1350
Gustavo Travassos <gutravassos@gmail.com>
2020-03-18 13:48:04 -0300

    Finished second fetch function

--- TheMovieDBObjc/TheMovieDBObjc/ViewController.m
+++ TheMovieDBObjc/TheMovieDBObjc/ViewController.m
@@ -12,7 +12,8 @@
 
 @interface ViewController ()
 
-@property (strong, nonatomic) NSMutableArray<Movie *> *movies;
+@property (strong, nonatomic) NSMutableArray<Movie *> *popularMovies;
+@property (strong, nonatomic) NSMutableArray<Movie *> *nowPlayingMovies;
 
 @end
 
@@ -21,18 +22,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    [self fetchMoviesUsingJSON];
-    // Do any additional setup after loading the view.
+    [self fetchPopularMovies];
+    [self fetchNowPlayingMovies];
 }
 
-- (void) setupMovies {
-    self.movies = NSMutableArray.new;
-}
-
-- (void) fetchMoviesUsingJSON {
-    NSLog(@"Fetching Movies...");
+- (void) fetchPopularMovies {
+    NSLog(@"Fetching popular movies...");
     
-    NSString *urlString = @"https://api.themoviedb.org/3/discover/movie?api_key=ca01e6658836c07edbe8b8ce2ac738c1&language=pt-BR&sort_by=popularity.desc&include_adult=false&include_video=false&page=1";
+    NSString *urlString = @"https://api.themoviedb.org/3/movie/popular?api_key=ca01e6658836c07edbe8b8ce2ac738c1&language=pt-BR&page=1";
     NSURL *url = [NSURL URLWithString:urlString];
     
     [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
@@ -66,13 +63,60 @@
             [movies addObject:movie];
         }
         
-        self.movies = movies;
-        
-        NSLog(@"Finished fetching courses!");
+        self.popularMovies = movies;
         
         for (Movie *movie in movies) {
             NSLog(@"%@", movie.title);
         }
+        
+        NSLog(@"Finished fetching popular movies!");
+    }] resume];
+}
+
+- (void) fetchNowPlayingMovies {
+    NSLog(@"Fetching now movies...");
+    
+    NSString *urlString = @"https://api.themoviedb.org/3/movie/now_playing?api_key=ca01e6658836c07edbe8b8ce2ac738c1&language=pt-BR&page=1";
+    NSURL *url = [NSURL URLWithString:urlString];
+    
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSError *err;
+        NSArray *results = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&err];
+        if (err){
+            NSLog(@"Failed to serialize into JSON: %@", err);
+            return;
+        }
+        
+        NSDictionary *queryDictionary = [results valueForKey:@"results"];
+        NSMutableArray<Movie *> *movies = NSMutableArray.new;
+        
+        for (NSDictionary *movieList in queryDictionary) {
+            NSString *title = movieList[@"title"];
+            NSString *description = movieList[@"overview"];
+            NSNumber *rate = movieList[@"vote_average"];
+            NSString *imageURL = movieList[@"poster_path"];
+//            NSString *category = movieList[@""]
+            
+            
+            Movie *movie = Movie.new;
+            movie.title = title;
+            movie.overview = description;
+            movie.resume = description;
+            movie.imageURL = imageURL;
+            movie.rate = rate;
+//            movie.category = category;
+
+            [movies addObject:movie];
+        }
+        
+        self.popularMovies = movies;
+        
+        for (Movie *movie in movies) {
+            NSLog(@"%@", movie.title);
+        }
+        
+        NSLog(@"Finished fetching now playing movies!");
         
     }] resume];
 }
